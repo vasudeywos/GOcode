452b2eb8a27d6ab236718f40889c96356cc420e4
vasudeywos <122253605+vasudeywos@users.noreply.github.com>
2023-09-07 15:44:52 +0530

    FUpdate task.go

--- task.go
+++ task.go
@@ -3,52 +3,79 @@ package main
 import (
 	"fmt"
 	"github.com/xuri/excelize/v2"
+	"strconv"
 )
-type student struct{
-	Name string
-	BITS_ID string
-	Email_Id string
-	Branch string
+type Student struct{
+	name string
+	bits_id string
+	email_id string
+	branch string
 }
-func (std student) getstd(){
-	fmt.Println(std.Name,std.BITS_ID,std.Email_Id,std.Branch)
+func(std Student) prntdetails(){
+	fmt.Println(std.name,std.bits_id,std.email_id,std.branch)
 }
-func (std student) prntdetails(){
-	fmt.Printf("Name:%v,BITS ID:%v,Email:%v,Branch:%v",std.Name,std.BITS_ID,std.Email_Id,std.Branch)
-}
+
 func main() {
     f,_ := excelize.OpenFile("Post_task1.xlsx")
 	
 	rows,_ := f.GetRows("Sheet1")
-	
-	var studentArr []student
 
-	studentInstance:=make(map[string]student)
-	
-	row1:=true
-	for _,row := range rows {
+	//studentArr is just a slice of all student strucs
+	var studentArr []Student
 
-		if row1==true{
-			row1=false
+	studentInstance:=make(map[string]Student)
+	//creating array from intial excel
+	for i,row := range rows {
+
+		if i==0{
 			continue
 		}
 		
-		stud:=student{
-			Name:row[1],
-			BITS_ID:row[0],
-			Email_Id:"f"+row[0][0:5]+row[0][8:11]+"@pilani.bits-pilani.ac.in",
-			Branch:row[0][4:6],
+		stud:=Student{
+			name:row[1],
+			bits_id:row[0],
+			email_id:"f"+row[0][0:5]+row[0][8:11]+"@pilani.bits-pilani.ac.in",
+			branch:row[0][4:6],
 		}
 		studentArr = append(studentArr, stud)
-		studentInstance[row[1]]=stud
+		studentInstance[row[0]]=stud
 	}
 
-	//studentArr is just a slice of all student strucs
-	for _,stdrw:=range studentArr{
-	 	stdrw.getstd()
-	 }
+	//creating newfile
+	newf:=excelize.NewFile()
+	index,_ := newf.NewSheet("Sheet1")
+	for i,studts:= range studentArr{
+		j:=strconv.Itoa(i)
+		newf.SetCellValue("Sheet1","A"+j,studts.bits_id)
+		newf.SetCellValue("Sheet1","B"+j,studts.name)
+		newf.SetCellValue("Sheet1","C"+j,studts.email_id)
+		newf.SetCellValue("Sheet1","D"+j,studts.branch)
+	} 
+	newf.SetActiveSheet(index)
+	if err := newf.SaveAs("NewPostSheet.xlsx"); err != nil {
+        fmt.Println(err)
+    }
+	
+
+	lastf,_ := excelize.OpenFile("Post_task1.xlsx")
+	
+	lrows,_ := lastf.GetRows("Sheet1")
+	for i,row := range lrows {
+
+		if i==0{
+			continue
+		}
+		
+		studer:=Student{
+			name:row[1],
+			bits_id:row[0],
+			email_id:row[2],
+			branch:row[3],
+		}
+		studentInstance[row[0]]=studer
+	}
 
 	//studentInstance is a map of of all student instances according to name
-	studentInstance["AARYAMAN"].prntdetails()
+	studentInstance["2022B2A71111P"].prntdetails()
 
 }
